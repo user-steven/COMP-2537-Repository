5da5ce336aa4827898362f91d8ebbadf27d5d782
Steven Chow <steven.chow@live.ca>
2022-05-13 01:36:26 -0700

    Reupload Assignment 2 files

---
+++ Assignment 2/public/js/timeline.js
@@ -0,0 +1,58 @@
+function loadEvents() {
+    $.ajax({
+        url: "http://localhost:5100/timeline/getAllEvents",
+        type: "get",
+        success: (data) => {
+            $("main").empty()
+            for (i=data.length - 1; i >= 0; i--) {
+                $("main").append(
+                    `
+                    <p>
+                        Event: ${data[i].search_event}
+                        <br>
+                        Time: ${data[i].time_event}
+                        <br>
+                        Likes: ${data[i].like_counter}
+                        <br>
+                        <span id="${data[i]._id}">
+                        <button type="button" class="likeButton"> Like </button>
+                        <button type="button" class="deleteButton"> Delete </button>
+                        </span>
+                    </p>
+                    `
+                )
+            }
+        }
+    })
+}
+
+function increaseLike() {
+    let timelineId = $(this).parent().attr('id')
+    // console.log(timelineId)
+    $.ajax({
+        url:`http://localhost:5100/timeline/update/${timelineId}`,
+        type: 'get',
+        success: result => console.log(result)
+    })
+    loadEvents()
+}
+
+function deletePost() {
+    let timelineId = $(this).parent().attr('id')
+    // console.log(timelineId)
+    $.ajax({
+        url:`http://localhost:5100/timeline/delete/${timelineId}`,
+        type: 'get',
+        success: result => console.log(result)
+    })
+    loadEvents()
+}
+
+function setup() {
+    loadEvents()
+    $("main").on('click', '.likeButton', increaseLike)
+    $("main").on('click', '.deleteButton', deletePost)
+}
+
+
+$(document).ready(setup)
---
+++ Assignment 2/server.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyparser = require("body-parser")
+const cors = require('cors')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('./public'))
+app.use(cors())
+app.use(bodyparser.urlencoded({
+    parameterLimit: 100000,
+    limit: '50mb',
+    extended: true
+}))
+
+app.listen(process.env.PORT || 5100, function (err) {
+    if (err)
+        console.log(err)
+})
+
+mongoose.connect("mongodb+srv://steven:qFoNHGmAl2u6YS4Z@cluster0.qcq3i.mongodb.net/PokedexClient?retryWrites=true&w=majority",
+ {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+const timelineSchema = new mongoose.Schema({
+     search_event: String,
+     time_event: String,
+     like_counter: Number
+ }, {
+     versionKey: false
+ })
+
+const timelineModel = mongoose.model("timeline", timelineSchema)
+
+app.get("/", (req, res) =>{
+    res.render(__dirname + "/public/index.ejs")
+})
+
+app.get("/timeline", (req, res) =>{
+    res.sendFile(__dirname + "/public/timeline.html")
+})
+
+// read
+ app.get('/timeline/getAllEvents', (req, res) =>{
+     timelineModel.find().then((result) =>{
+         res.send(result)
+     }).catch((err) =>{
+         console.log(err)
+     })
+ })
+
+// create
+ app.put('/timeline/insert', (req, res) =>{
+    timelineModel.create({
+        search_event: req.body.search_event,
+        time_event: req.body.time_event,
+        like_counter: req.body.like_counter
+    }).then((result) => {
+        console.log(result)
+        res.send("Created successfully")
+    }).catch((err) =>{
+        console.log(err)
+    })
+})
+
+// update
+app.get('/timeline/update/:id', (req, res) =>{
+    timelineModel.updateOne({
+        "_id" : req.params.id
+    }, {
+        $inc: {like_counter: 1}
+    }).then((result) =>{
+        console.log(result)
+        res.send("Updated successfully")
+    }).catch((err) =>{
+        console.log(err)
+    })
+})
+
+// delete
+app.get('/timeline/delete/:id', (req, res) =>{
+    timelineModel.remove({
+        "_id" : req.params.id
+    }).then((result) =>{
+        console.log(result)
+        res.send("Deleted successfully")
+    }).catch((err) =>{
+        console.log(err)
+    })
+})
