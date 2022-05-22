d5fbb99c5f22f5f3298215179949e38bbcd843ac
Steven Chow <steven.chow@live.ca>
2022-05-22 11:47:41 -0700

    Complete cart and order portion of database

--- Assignment 3/server.js
+++ Assignment 3/server.js
@@ -38,6 +38,8 @@ mongoose.connect("mongodb+srv://steven:qFoNHGmAl2u6YS4Z@cluster0.qcq3i.mongodb.n
 
 
 const timelineSchema = new mongoose.Schema({
+    userId: String,
+    name: String,
     search_event: String,
     time_event: String,
     like_counter: Number
@@ -50,30 +52,52 @@ const userSchema = new mongoose.Schema({
     lastName: String,
     email: { type: String, required: true, unique: true },
     password: String,
-    admin: Boolean,
-    cart: Array,
-    orders: Array
+    admin: Boolean
+}, {
+    versionKey: false
+})
+
+const cartSchema = new mongoose.Schema({
+    userId: String,
+    pokemonId: Number,
+    imgSource: String,
+    pokemonName: String,
+    backgroundColor: String,
+    price: Number,
+    quantity: Number
+}, {
+    versionKey: false
+})
+
+const orderSchema = new mongoose.Schema({
+    userId: String,
+    date: Date,
+    order: {}
 }, {
     versionKey: false
 })
 
 const timelineModel = mongoose.model("timeline", timelineSchema)
 const userModel = mongoose.model("user", userSchema)
+const cartModel = mongoose.model("cart", cartSchema)
+const orderModel = mongoose.model("order", orderSchema)
 
-app.get("/", authorization, (req, res) => {
+app.get("/", (req, res) => {
     res.render(__dirname + "/public/index.ejs")
 })
 
-app.get("/profile", authorization, async (req, res) => {
-    user = await userModel.findOne({ _id: req.session.user })
-
+app.get("/profile", authorization, (req, res) => {
     res.render(__dirname + "/public/profile.ejs", {
-        name: user.firstName,
-        cart: user.cart,
-        orders: user.orders
+        name: req.session.name
     })
 })
 
+app.get("/cart", authorization, async (req, res) => {
+    cart = await cartModel.find({ userId: req.session.user })
+    res.render(__dirname + "/public/cart.ejs", {
+        cart: cart
+    })
+})
 
 app.get("/login", (req, res) => {
     if (req.session.authenticated)
@@ -97,9 +121,7 @@ app.post("/api/register", async (req, res) => {
         lastName: req.body.lastName,
         email: req.body.email.toLowerCase().trim(),
         password: hash_password,
-        admin: false,
-        cart: [],
-        orders: []
+        admin: false
     }).then((result) => {
         // console.log(result)
         res.redirect("/login")
@@ -114,12 +136,13 @@ app.post("/api/login", (req, res) => {
     email = req.body.email.toLowerCase().trim()
     password = req.body.password
 
-    userModel.exists({email}, async (err, result) => {
+    userModel.exists({ email }, async (err, result) => {
         if (result) {
             const user = await userModel.findOne({ email })
             if (await bcrypt.compare(password, user.password)) {
                 req.session.authenticated = true
                 req.session.user = user.id
+                req.session.name = user.firstName
                 console.log("Login successful")
                 res.redirect("/profile")
             }
@@ -140,12 +163,80 @@ app.get("/logout", (req, res) => {
 })
 
 app.get("/timeline", (req, res) => {
-    res.render(__dirname + "/public/timeline.ejs")
+    res.render(__dirname + "/public/timeline.ejs", {
+        name: req.session.name
+    })
 })
 
+app.post("/api/addToCart", async (req, res) => {
+
+    await cartModel.exists({
+        $and: [{
+            userId: req.session.user
+        }, {
+            pokemonId: req.body.pokemonId
+        }]
+    }, async (err, result) => {
+        if (result) {
+            await cartModel.findOneAndUpdate(
+                { $and: [{ userId: req.session.user }, { pokemonId: req.body.pokemonId }] },
+                { $inc: { quantity: req.body.quantity } }
+            )
+            console.log("Item already exists in cart. Updating quantity.")
+            res.status(204).send()
+        } else {
+            await cartModel.create(
+                {
+                    userId: req.session.user,
+                    pokemonId: req.body.pokemonId,
+                    imgSource: req.body.imgSource,
+                    pokemonName: req.body.pokemonName,
+                    backgroundColor: req.body.backgroundColor,
+                    price: req.body.price,
+                    quantity: req.body.quantity
+                }
+            )
+            console.log("Item added to cart.")
+            res.status(204).send()
+        }
+    })
+})
+
+app.get("/api/checkout", authorization, async (req, res) =>{
+    checkoutItems = await cartModel.find({ 
+        userId: req.session.user
+    },{
+        userId: 0,
+        _id: 0
+    })
+    await orderModel.create({
+        userId: req.session.user,
+        date: new Date(),
+        order: checkoutItems
+    })
+    await cartModel.deleteMany({
+        userId: req.session.user
+    })
+    res.render(__dirname + "/public/thankyou.ejs")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 // read
-app.get('/timeline/getAllEvents', (req, res) => {
-    timelineModel.find().then((result) => {
+app.get('/timeline/getAllEvents', authorization, async (req, res) => {
+    await timelineModel.find({ 
+        userId: req.session.user
+    }).then((result) => {
         res.send(result)
     }).catch((err) => {
         console.log(err)
@@ -153,21 +244,23 @@ app.get('/timeline/getAllEvents', (req, res) => {
 })
 
 // create
-app.put('/timeline/insert', (req, res) => {
+app.put('/timeline/insert', authorization, (req, res) => {
     timelineModel.create({
+        userId: req.session.user,
+        name: req.session.name,
         search_event: req.body.search_event,
         time_event: req.body.time_event,
         like_counter: req.body.like_counter
     }).then((result) => {
         // console.log(result)
-        res.send("Client search has been logged successfully")
+        res.send("Client action has been logged successfully")
     }).catch((err) => {
         console.log(err)
     })
 })
 
 // update
-app.get('/timeline/update/:id', (req, res) => {
+app.get('/timeline/update/:id', authorization, (req, res) => {
     timelineModel.updateOne({
         "_id": req.params.id
     }, {
@@ -181,7 +274,7 @@ app.get('/timeline/update/:id', (req, res) => {
 })
 
 // delete
-app.get('/timeline/delete/:id', (req, res) => {
+app.get('/timeline/delete/:id', authorization, (req, res) => {
     timelineModel.remove({
         "_id": req.params.id
     }).then((result) => {
