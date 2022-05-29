1eedd4d809324b27597dd9446b4a1cbc76911b9d
Steven Chow <steven.chow@live.ca>
2022-05-29 02:17:40 -0700

    Create basic game logic

--- Assignment 4/public/js/memorygame.js
+++ Assignment 4/public/js/memorygame.js
@@ -1,21 +1,130 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${}.png
+var firstCard = null
+var secondCard = null
+var firstCardFlipped = false
+
+var gameWidth = null
+var gameHeight = null
+var pokemonCount = null
+var randomPokemon = null
+var neededPokemonCount = null
+var pokemonCardArray = null
 
 
 function gameOption() {
     $("#gameOption").submit((event) => {
         event.preventDefault()
+
+        gameWidth = parseInt($("#width").val())
+        gameHeight = parseInt($("#height").val())
+        pokemonCount = parseInt($("#pokemonCount").val())
+        randomPokemon = generateRandomPokemon(pokemonCount)
+        neededPokemonCount = (gameWidth * gameHeight) / 2
+        pokemonCardArray = shuffleArray(generatePokemonCards(randomPokemon, neededPokemonCount, gameWidth, gameHeight))
+        $("#gameGrid").html(pokemonCardArray)
+        $(".card").on("click", flip)
     })
+}
 
-    let gameWidth = parseInt($("#width").val())
-    let gameHeight = parseInt($("#height").val())
-    let pokemonCount = parseInt($("#pokemonCount").val())
+function generatePokemonCards(pokemonId, requiredPokemonCount, width, height) {
+    let count = 0
+    let index = 0
+    let pokemonCardArray = []
 
+    while (count < requiredPokemonCount) {
+        if (index == pokemonId.length) {
+            index = 0
+        } else {
+            // console.log("count:" + count + " pokemon ID: " + randomPokemon[index])
+            card_1 = `
+            <div class="card" style="height: calc(${100/height}% - 5px); width: calc(${100/width}% - 5px);">
+                <img class="frontFace" id="${pokemonId[index]}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId[index]}.png">
+                <img class="backFace" src="./images/back.png">
+            </div>`
+            card_2 = `
+            <div class="card" style="height: calc(${100/height}% - 5px); width: calc(${100/width}% - 5px);">
+                <img class="frontFace" id="${pokemonId[index] + 1000}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId[index]}.png">
+                <img class="backFace" src="./images/back.png">
+            </div>`
+            pokemonCardArray.push(card_1)
+            pokemonCardArray.push(card_2)
+            index++
+            count++
+        }
+    }
+    return pokemonCardArray
+}
+
+function generateRandomPokemon(count) {
+    let set = new Set()
+    while (set.size < count) {
+        set.add(Math.floor(Math.random() * (899 - 1) + 1))
+    }
+    return Array.from(set)
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array
+}
+
+function flip() {
+    $(this).toggleClass("flip")
+    $(this).off("click")
+    if (!firstCardFlipped) {
+        firstCard = $(this).find(".frontFace")[0]
+        firstCardFlipped = true
+    } else {
+        secondCard = $(this).find(".frontFace")[0]
+        firstCardFlipped = false
+        checkCards(firstCard, secondCard)
+    }
+}
+
+function checkCards(firstCard, secondCard) {
+    $("#gameGrid").addClass("disableClick")
+    if (
+        $(`#${firstCard.id}`).attr("src")
+        ==
+        $(`#${secondCard.id}`).attr("src")
+    ) {
+        console.log("a match!")
+        $(`#${firstCard.id}`).parent().off("click")
+        $(`#${secondCard.id}`).parent().off("click")
+        $("#gameGrid").removeClass("disableClick")
+        setTimeout(() => {
+            if (document.getElementsByClassName("flip").length == (gameWidth * gameHeight)) {
+                alert("You win!")
+            }
+        }, 1500)
+
+    } else {
+        setTimeout(() => {
+            $(`#${firstCard.id}`).parent().removeClass("flip")
+            $(`#${secondCard.id}`).parent().removeClass("flip")
+            $(`#${firstCard.id}`).parent().on("click", flip)
+            $(`#${secondCard.id}`).parent().on("click", flip)
+            $("#gameGrid").removeClass("disableClick")
+        }, 2000)
+    }
     
 }
 
+function handler(event) {
+    if (event.target.className == "card") {
+        event.stopPropagation()
+        event.preventDefault()
+    }
+}
 
 function setup() {
-    document.getElementById("gameOptionButton").addEventListener("click", gameOption)
+    $("#gameOptionButton").on("click", gameOption)
+    
 }
 
 $(document).ready(setup)
